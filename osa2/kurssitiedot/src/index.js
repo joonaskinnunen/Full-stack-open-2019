85a444e073f4af6d34ecf5027c607cc16be158ee
”Joonas <joonas.kinn@gmail.com>
2019-09-06 20:03:06 +0300

    Add task 2.1

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = props =>
+  <h1>{props.course}</h1>
+
+const Part = props =>
+  <p>{props.part.name} {props.part.exercises}</p>
+
+const Content = props => (
+  <div>
+    {props.parts.map((x, i) => <Part part={x} key={i} />)}
+  </div>
+)
+
+const Course = props => {
+  return (
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
